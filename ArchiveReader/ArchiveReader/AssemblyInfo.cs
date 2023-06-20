592f2e55bba124b042581476a377a0ea44751569
Troseberry <tayroseberry@gmail.com>
2023-06-19 22:44:41 -0700

    Updated work data template and default (light) color scheme

--- ArchiveReader/ArchiveReader/AssemblyInfo.cs
+++ ArchiveReader/ArchiveReader/AssemblyInfo.cs
@@ -7,3 +7,13 @@ using Xamarin.Forms.Xaml;
 [assembly: ExportFont("OpenSans-Regular.ttf", Alias = "OpenSans")]
 [assembly: ExportFont("OpenSans-Italic.ttf", Alias = "OpenSans_Italic")]
 [assembly: ExportFont("OpenSans-Bold.ttf", Alias = "OpenSans_Bold")]
+
+
+[assembly: ExportFont("Lato-Bold.ttf", Alias = "Lato_Bold")]
+[assembly: ExportFont("Lato-BoldItalic.ttf", Alias = "Lato_BoldItalic")]
+[assembly: ExportFont("Lato-Italic.ttf", Alias = "Lato_Italic")]
+[assembly: ExportFont("Lato-Light.ttf", Alias = "Lato_Light")]
+[assembly: ExportFont("Lato-LightItalic.ttf", Alias = "Lato_LightItalic")]
+[assembly: ExportFont("Lato-Regular.ttf", Alias = "Lato_Regular")]
+[assembly: ExportFont("Lato-Thin.ttf", Alias = "Lato_Thin")]
+[assembly: ExportFont("Lato-ThinItalic.ttf", Alias = "Lato_ThinItalic")]
