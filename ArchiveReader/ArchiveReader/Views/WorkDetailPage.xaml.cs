e2413379e05f729503130ddf2d8a74a62786db37
Troseberry <tayroseberry@gmail.com>
2023-06-21 01:25:04 -0700

    Updating work details page to new UI

--- ArchiveReader/ArchiveReader/Views/WorkDetailPage.xaml.cs
+++ ArchiveReader/ArchiveReader/Views/WorkDetailPage.xaml.cs
@@ -12,13 +12,22 @@ using ArchiveReader.Data;
 
 using System.Text.Json;
 using System.Diagnostics;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace ArchiveReader.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WorkDetailPage : ContentPage
     {
-        private Work currentWork;
+        private string _apiFunctionPath;
+
+        private HttpClient _client;
+
+        private Work _currentWork;
+        private ObservableCollection<Chapter> _chapters;
+
         public WorkDetailPage()
         {
             InitializeComponent();
@@ -28,20 +37,98 @@ namespace ArchiveReader.Views
         {
             InitializeComponent();
 
-            currentWork = workToDisplay;
-            BindingContext = workToDisplay;
+            _client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://ao3api.netlify.app/.netlify/functions/"),
+            };
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            _apiFunctionPath = "";
+
+            _currentWork = workToDisplay;
+
+            BindingContext = _currentWork;
         }
 
-        private async void ReadButton_Pressed(object sender, EventArgs e)
+        protected override void OnBindingContextChanged()
         {
-            await Navigation.PushAsync(new ReaderPage(currentWork));
+            base.OnBindingContextChanged();
+
+            if (_currentWork != null) SetupDetails();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (_currentWork != null) SetupDetails();
+        }
+
+        //To-Do: figure out why bindable layout won't fully populate all chapters unless
+        //SetItemsSource gets called twice (only fixes some instances)
+        private async void SetupDetails()
+        {
+            await HandleChapters();
+            BindableLayout.SetItemsSource(ChaptersLayout, _chapters);
+        }
+
+        private async Task<bool> HandleChapters()
+        {
+            _apiFunctionPath = $"GetChaptersForFanfic?workId={_currentWork.id}&lastId={_currentWork.latestChapterId}";
+            //Debug.WriteLine("[HandleChapters] - Api Func Path: " + _apiFunctionPath);
+
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(_apiFunctionPath);
+                if (response.IsSuccessStatusCode)
+                {
+                    string contentString = await response.Content.ReadAsStringAsync();
+                    List<Chapter> outputChapters = JsonSerializer.Deserialize<List<Chapter>>(contentString);
+
+                    _chapters = new ObservableCollection<Chapter>();
+
+                    for (int i = outputChapters.Count - 1; i >= 0; i--)
+                    {
+                        var chapter = outputChapters[i];
+
+                        //int splitIndex = chapter.title.IndexOf('.');
+                        //chapter.title = chapter.title.Substring(splitIndex + 1);
+
+                        _chapters.Add(chapter);
+                    }
+
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        private async void ReadFromBeginningButton_Pressed(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ReaderPage(_currentWork));
         }
 
         private async void SaveButton_Pressed(object sender, EventArgs e)
         {
             LibraryDatabase db = await LibraryDatabase.Instance;
 
-            await db.SaveWorkAsync(currentWork);
+            await db.SaveWorkAsync(_currentWork);
+        }
+
+        private void ChaptersListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            //chaptersListView.SelectedItem = null;
+        }
+
+        private void ChaptersListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            
         }
     }
 }
