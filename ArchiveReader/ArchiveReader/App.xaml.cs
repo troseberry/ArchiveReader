8c28a6181ad48953d09c79eaf2d0b9379cb00a6e
Troseberry <tayroseberry@gmail.com>
2023-06-21 22:18:18 -0700

    Fixed chapters list issue in WorkDetailsPage

--- ArchiveReader/ArchiveReader/App.xaml.cs
+++ ArchiveReader/ArchiveReader/App.xaml.cs
@@ -8,7 +8,6 @@ namespace ArchiveReader
 {
     public partial class App : Application
     {
-
         public App()
         {
             InitializeComponent();
--- ArchiveReader/ArchiveReader/Views/WorkDetailPage.xaml.cs
+++ ArchiveReader/ArchiveReader/Views/WorkDetailPage.xaml.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-using ArchiveReader.Models;
-using ArchiveReader.Data;
-
 using System.Text.Json;
 using System.Diagnostics;
 using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Net.Http.Headers;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Shapes;
+
+using ArchiveReader.Models;
+using ArchiveReader.Data;
+using ArchiveReader;
+
 
 namespace ArchiveReader.Views
 {
@@ -27,6 +29,8 @@ namespace ArchiveReader.Views
 
         private Work _currentWork;
         private ObservableCollection<Chapter> _chapters;
+
+        private Color _primaryFont;
 
         public WorkDetailPage()
         {
@@ -58,19 +62,14 @@ namespace ArchiveReader.Views
             if (_currentWork != null) SetupDetails();
         }
 
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-
-            if (_currentWork != null) SetupDetails();
-        }
-
-        //To-Do: figure out why bindable layout won't fully populate all chapters unless
-        //SetItemsSource gets called twice (only fixes some instances)
         private async void SetupDetails()
         {
+            if (Application.Current.Resources.TryGetValue("PrimaryFont", out object value))
+            {
+                _primaryFont = (Color)value;
+            }
+
             await HandleChapters();
-            BindableLayout.SetItemsSource(ChaptersLayout, _chapters);
         }
 
         private async Task<bool> HandleChapters()
@@ -83,6 +82,7 @@ namespace ArchiveReader.Views
                 HttpResponseMessage response = await _client.GetAsync(_apiFunctionPath);
                 if (response.IsSuccessStatusCode)
                 {
+
                     string contentString = await response.Content.ReadAsStringAsync();
                     List<Chapter> outputChapters = JsonSerializer.Deserialize<List<Chapter>>(contentString);
 
@@ -96,7 +96,11 @@ namespace ArchiveReader.Views
                         //chapter.title = chapter.title.Substring(splitIndex + 1);
 
                         _chapters.Add(chapter);
+
+                        contentLayout.Children.Add(CreateChapterLayout(chapter));
                     }
+
+                    pageScrollView.ForceLayout();
 
                     return true;
                 }
@@ -129,6 +133,32 @@ namespace ArchiveReader.Views
         private void ChaptersListView_ItemTapped(object sender, ItemTappedEventArgs e)
         {
             
+        }
+
+        private StackLayout CreateChapterLayout(Chapter chapterToBind)
+        {
+            Label titleLabel = new Label() 
+            { 
+                FontSize = 18,
+                Padding = new Thickness(10, 0), 
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                TextColor = _primaryFont,
+                Text = chapterToBind.title
+            };
+
+            Line divider = new Line() { HeightRequest = 2, BackgroundColor = _primaryFont };
+
+            StackLayout layout = new StackLayout()
+            {
+                HeightRequest = 60,
+                Children =
+                {
+                    titleLabel,
+                    divider
+                }
+            };
+
+            return layout;
         }
     }
 }
